dbada00a49655c752bed9945da562a1104058ba3
Valerie Jane <valeriejanethoma@gmail.com>
2018-02-04 16:18:25 -0500

    tdd

---
+++ tdd.js
@@ -0,0 +1,8 @@
+// 1. write the tests 
+// 2. see the tests fail 
+// 3. write code to pass the tests 
+// 4. refactor code as neccesary 
+// 5. repeat
+
+// not universally accepted, development process is longer, but code base is more likely to 
+// remain bug free and maintainable as the application grows
